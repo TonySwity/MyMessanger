7de6036ab3931e89a20d688205d2ada1ef3a2514
TonySwity <goldocean@live.com>
2022-10-02 13:36:50 +0300

    Установил NuGet пакет Newtonsoft serialize and deserialize Json

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,14 @@
-﻿using MyMessanger;
+﻿using Newtonsoft.Json;
+using MyMessanger;
 
-Message msg = new Message();
-Console.WriteLine(msg.ToString());
+Message msg = new Message("Tony", "Privet", DateTime.UtcNow);
+string output = JsonConvert.SerializeObject(msg);
+Console.WriteLine(output);
+Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+Console.WriteLine(deserializeMsg);
+
+/*{ "UserName":"Tony","MessageText":"Privet","TimeStamp":"2022-10-02T10:31:51.10571Z"}
+Tony < 02.10.2022 10:31:51 >: Privet*/
+
+
+
