aa65a3a8d4e09666f4bc2371d9e07a0d5013a925
TonySwity <goldocean@live.com>
2022-10-02 12:51:02 +0300

    Добавил класс Message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MyMessanger;
+
+Message msg = new Message();
+Console.WriteLine(msg.ToString());
